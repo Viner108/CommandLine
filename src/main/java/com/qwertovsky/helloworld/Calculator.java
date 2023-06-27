f0f6e3a3d942a967f2370bd5f544f16571b7534e
Viner108 <andrukovichl@mail.ru>
2023-06-27 15:48:31 +0300

    Initial commit

---
+++ src/main/java/com/qwertovsky/helloworld/Calculator.java
@@ -0,0 +1,11 @@
+package com.qwertovsky.helloworld;
+
+public class Calculator {
+    public int sum(int... a) {
+        Adder adder = new Adder();
+        for (int i : a) {
+            adder.add(i);
+        }
+        return adder.getSum();
+    }
+}
